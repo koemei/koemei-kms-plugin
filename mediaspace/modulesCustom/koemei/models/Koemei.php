b39ed1d2ecf11b67f9573a57cc4e2fcc5b425ff4
Traian Tarca <traian@webarhitect.eu>
2013-06-05 16:18:52 +0300

    Admin

--- mediaspace/modulesCustom/koemei/models/Koemei.php
+++ mediaspace/modulesCustom/koemei/models/Koemei.php
@@ -8,8 +8,10 @@
  
 
 
-class Koemei_Model_Koemei extends Kms_Module_BaseModel implements Kms_Interface_Deployable_PreDeployment,Kms_Interface_Model_Dependency {
+class Koemei_Model_Koemei extends Kms_Module_BaseModel implements Kms_Interface_Deployable_PreDeployment {
 	const MODULE_NAME = 'Koemei';
+	
+	private $canEnable;
     public $viewHooks = array
     (
 
@@ -63,18 +65,41 @@ class Koemei_Model_Koemei extends Kms_Module_BaseModel implements Kms_Interface_
 	
 	public function canEnable()
     {
-        return false;
+		$uuid = Kms_Resource_Config::getModuleConfig('koemei', 'Koemei_uuid');
+		if (!$uuid) {
+			$this->canEnable = false;	
+		} else {
+			$this->canEnable = true;		
+		}
+		return $this->canEnable;	
     }
 	
 	 public function getPreDeploymentFailReason()
     {
-        return '<br><br>Deployment should be allowed. You shouldn\'t ever see this message<br>';
+        return "<br><br>Please specify your Koemei UUID before enabling the Module.<br><br>If you don't have an account please go to <a href=\"https://www.koemei.com/billing\" target='_blank'>Koemei website</a> and create your account<br>";
     }
-	
-	public static function getModuleDependency() {
-		return array('captions');
+	public static function get_data($url) {
+		$ch = curl_init();
+		$timeout = 5;
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+		$data = curl_exec($ch);
+		curl_close($ch);
+		return $data;
 	}
-
+	
+	
+	public static function settingsSaved() {
+		$k_id= $_POST['Koemei_uuid'];
+		$content = Koemei_Model_Koemei::get_data('https://www.koemei.com/REST/users/'.$k_id);
+		$xml = simplexml_load_string($content);
+		if (!isset($xml->Id)) {
+			echo "The Koemei UUID you've specified dose not exist.";	
+			exit;
+		}
+		
+	}
 
 }
 ?>
